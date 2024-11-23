0bcd2c51d17e156c4a4acfb31128e3baa3aa8f60
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2024-11-23 17:57:12 +0530

    updated api.js

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://homyz-real-estate-project.vercel.app/api",
+  baseURL: "https://homyz-real-estate-project-server.vercel.app/api",
   // baseURL: "http://localhost:8000/api",
 });
 
